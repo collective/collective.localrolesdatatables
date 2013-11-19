1eb4b76aff54aed71d3538afb1ce75ba8df8e5bc
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-11-19 17:57:23 +0100

    fix typo in setup.py [test]

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(name='collective.localrolesdatatables',
           'collective.js.datatables',
           # -*- Extra requirements: -*-
       ],
-      extras_require=dict(tests=['plone.app.testing']),
+      extras_require=dict(test=['plone.app.testing']),
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
